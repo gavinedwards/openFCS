cb47e153df37d0e46188605862263342428ad0d7
Gavin Edwards <edwards.gavin@gmail.com>
2018-09-13 20:34:10 -0700

    Got the CANcontrollers hooked up to arduino UNO R3 boards and talking to eachother

---
+++ lru/pitch_controller/pitch_controller.ino
@@ -0,0 +1,72 @@
+// demo: CAN-BUS Shield, receive data with check mode
+// send data coming to fast, such as less than 10ms, you can use this way
+// loovee, 2014-6-13
+
+
+#include <SPI.h>
+#include "mcp_can.h"
+#include <LiquidCrystal.h>
+
+
+// Initialise LCD
+
+const int rs = 8, en = 10, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+// the cs pin of the version after v1.1 is default to D9
+// v0.9b and v1.0 is default D10
+const int SPI_CS_PIN = 9;
+
+MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
+
+void setup()
+{
+    // set up the LCD's number of columns and rows:
+    lcd.begin(16, 2);
+    // Print a message to the LCD.
+    lcd.print("Pitch   Yaw");
+    
+    Serial.begin(115200);
+
+    while (CAN_OK != CAN.begin(CAN_500KBPS))              // init can bus : baudrate = 500k
+    {
+        Serial.println("CAN BUS Shield init fail");
+        Serial.println(" Init CAN BUS Shield again");
+        delay(100);
+    }
+    Serial.println("CAN BUS Shield init ok!");
+}
+
+void loop()
+{
+    unsigned char len = 0;
+    unsigned char buf[8];
+    
+    if(CAN_MSGAVAIL == CAN.checkReceive())            // check if data coming
+    {
+        CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
+
+        unsigned long canId = CAN.getCanId();
+        
+        Serial.println("-----------------------------");
+        Serial.print("Get data from ID: 0x");
+        Serial.println(canId, HEX);
+
+        for(int i = 0; i<len; i++)    // print the data
+        {
+            Serial.print(buf[i], HEX);
+            Serial.print("\t");
+        }
+        Serial.println();
+
+        if(canId == 0x134){
+            lcd.setCursor(0, 1);
+            lcd.print((10 * buf [6]) + buf[7], DEC);          
+        }
+        if(canId == 0x199){
+            lcd.setCursor(8, 1);
+            lcd.print((10 * buf [6]) + buf[7], DEC);          
+        }
+    }
+}
+
